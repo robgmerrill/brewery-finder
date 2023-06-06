1c9493cc86e81cd47b548cfa2c6321a304dbac2e
Rob Merrill <robgmerrill@gmail.com>
2023-06-06 19:20:38 +0000

    add breweryAPI search field

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,21 +3,49 @@ import { useNavigate } from 'react-router-dom';
 import AppContext from '../components/AppContext';
 import './Home.css';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Home() {
+  const [breweries, setBreweries] = useState([]);
+  const [zipCode, setZipCode] = useState('');
   const { user } = useContext(AppContext);
   const navigate = useNavigate();
+  console.log(zipCode);
 
   // useEffect(() => {
   //   if (!user) navigate('/sign-in');
   // }, [user, navigate]);
 
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `https://api.openbrewerydb.org/v1/breweries?by_postal=98108`
+      );
+      const data = await response.json();
+      console.log(data);
+    }
+    fetchData();
+  });
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    const response = await fetch(
+      `https://api.openbrewerydb.org/v1/breweries?by_postal=${zipCode}`
+    );
+    const data = await response.json();
+    console.log(data);
+  }
+
   return (
     <div className="d-flex flex-column min-vh-100 justify-content-center align-items-center">
-      <h2>Learn React</h2>
-      <Link to="/exercises/jsx">JSX</Link>
-      <h3>Fundamentals</h3>
-      <h3>Props and State</h3>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="zip-code">enter zip</label>
+        <input
+          value={zipCode}
+          onChange={(e) => setZipCode(e.target.value)}
+          id="zip-code"
+        />
+      </form>
     </div>
   );
 }
